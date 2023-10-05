8b400e2cd16541607675b3a290c8ad4bd07ce8cc
Samur18 <samurijks@gmail.com>
2023-10-05 17:35:27 +0200

    Initial commit

---
+++ Headers/include/MyEnum.h
@@ -0,0 +1,31 @@
+//
+// Created by samur on 05/10/2023.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef P1_SHUNDONGWANG_SAMUELRIJKS_MYENUM_H
+#define P1_SHUNDONGWANG_SAMUELRIJKS_MYENUM_H
+
+
+namespace MyEnum {
+    enum eDirection { Up, Down, Left, Rigth};
+    const string nameDir[] = { "Up", "Down", "Left", "Rigth" };
+    const string symbolDir[] = { " ^", " v", " <", " >" };
+    const int length = 4;
+    const int moveDir[2][length] = {-1, 1, 0, 0, //row
+                                    0, 0, -1, 1}; //col
+
+    string toName( eDirection dir);
+
+    string toSymbol( eDirection dir);
+
+    eDirection giveDirection();
+
+    int* movement(eDirection dir);
+};
+
+
+#endif //P1_SHUNDONGWANG_SAMUELRIJKS_MYENUM_H
---
+++ Sources/src/MyEnum.cpp
@@ -0,0 +1,36 @@
+//
+// Created by samur on 05/10/2023.
+//
+
+#include "../../Headers/include/MyEnum.h"
+
+string MyEnum::toName( eDirection dir)
+{
+    return nameDir[dir];
+}
+
+string MyEnum::toSymbol( eDirection dir)
+{
+    return symbolDir[dir];
+}
+
+MyEnum::eDirection MyEnum::giveDirection(){
+    int valor, min = 1;
+    cout << "Escull una direccio:" << endl;
+    for( int i = min; i <= length; i++)
+        cout << "\t" << i << " " << nameDir[i-1] << endl;
+
+    do{
+        cout << "Especifica un valor dins de l'interval [" << min << ", " << length << "]" << endl;
+        cin >> valor;
+    } while (valor < min || valor > length);
+
+    return eDirection(valor-1);
+}
+
+int* MyEnum::movement(MyEnum::eDirection dir) {
+    static int pos[2];
+    pos[0] = moveDir[0][dir];
+    pos[1] = moveDir[1][dir];
+    return pos;
+}
